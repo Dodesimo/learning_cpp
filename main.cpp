4561767fc614343955bb5c896102d925c2ac7209
devam <devammondal06@gmail.com>
2025-11-29 17:57:13 -0500

    simple calc

--- main.cpp
+++ main.cpp
@@ -2,7 +2,17 @@
 
 int main() {
 
-   int x = 6;
-   std::cout << x << std::endl;
-   std::cout << x;
+   //basic calculator
+   int firstNumber {};
+   int secondNumber {};
+   int sum {};
+
+   std::cout << "Enter your first number";
+   std::cin >> firstNumber;
+   std::cout << "Enter your second number";
+   std::cin >> secondNumber;
+   sum = firstNumber + secondNumber;
+   std::cout << sum;
+
+
 }

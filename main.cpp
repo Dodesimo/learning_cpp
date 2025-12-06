15164234a36ff510f256c6cceec8c45a7526c82e
devam <devammondal06@gmail.com>
2025-12-05 23:09:48 -0500

    ascii program

--- main.cpp
+++ main.cpp
@@ -3,9 +3,8 @@
 #include "check.h"
 
 int main() {
-    float a {12.1232123};
-    int b {static_cast<int>(a)};
-    std::cout << a;
-    std::cout << '\n';
-    std::cout << b;
+    int8_t character {};
+    std::cout << "Enter a single character. ";
+    std::cin >> character;
+    std::cout << "You entered " << character << " , " << "which has an ASCII code of " << static_cast<int>(character);
 }

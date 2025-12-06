978fc85c553dac9eff15a91ad7d2d2c288727ba6
devam <devammondal06@gmail.com>
2025-12-06 13:16:51 -0500

    bitset=

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
+#include <bitset>
 #include <iomanip>
 #include <iostream>
 #include "check.h"
 
 int main() {
-    int8_t character {};
-    std::cout << "Enter a single character. ";
-    std::cin >> character;
-    std::cout << "You entered " << character << " , " << "which has an ASCII code of " << static_cast<int>(character);
+    std::bitset<8> a {5};
+    std::cout << a;
 }

efc0c7e760d5a3ac15999f89862ff81997ab738f
devam <devammondal06@gmail.com>
2025-12-08 18:55:31 -0500

    namespace conflict

--- main.cpp
+++ main.cpp
@@ -1,27 +1,10 @@
 #include <iostream>
-#include <cmath>
 
-bool isEven(int n) {
-
-    if (n % 2 == 1) {
-
-        return false;
-
-    } else {
-
-        return true;
-
-    }
-}
-
-
-
+int doSomething(int a, int b);
 
 int main() {
 
-    int n {};
-    std::cout << "Enter a number:";
-    std::cin >> n;
-    std::cout << (isEven(n) ? "Your number is even!" : "Your number is odd!");
+    std::cout << doSomething(3, 2);
+    return 0;
 
 }

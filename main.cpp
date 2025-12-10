011cd16d50ec93272954b1c0bad3e90f706e6e78
devam <devammondal06@gmail.com>
2025-12-10 17:16:27 -0500

    while loop and see odd/even

--- main.cpp
+++ main.cpp
@@ -1,27 +1,24 @@
 #include <iostream>
 #include <string.h>
 
-void returnString (int x) {
-
-    switch (x) {
-
-        case 1:
-            std::cout << std::string {"One"};
-        case 2:
-            std::cout << std::string {"Two"};
-            [[fallthrough]];
-        case 3:
-            std::cout << std::string {"Three"};
-        default:
-            std::cout << std::string{"Not real"};
-
-    }
-
-}
-
 int main() {
 
-    returnString(2);
-    return 0;
+    int count {1};
+
+    while (count < 11) {
+
+        if (count % 2 == 1) {
+
+            std::cout << "odd\n";
+
+        } else {
+
+            std::cout << "even\n";
+
+        }
+
+        count += 1;
+
+    }
 
 }

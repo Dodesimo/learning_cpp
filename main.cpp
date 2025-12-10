5820fc6a18f1b30095d3b546eb97e63f33911845
devam <devammondal06@gmail.com>
2025-12-10 17:44:16 -0500

    get into the habit of prefix incrementation

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
         int i {0}; //this is required because the counter variable loses scope outside loop
         while (i < 10) {
             std::cout << i;
-            i += 1;
+            ++i;
         }
 
     }

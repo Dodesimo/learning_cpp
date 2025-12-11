baf045ba5c95cd0d1fcd768770f57ba98ba46f1a
devam <devammondal06@gmail.com>
2025-12-10 19:09:03 -0500

    use std::random_device to get a unique random number

--- main.cpp
+++ main.cpp
@@ -3,9 +3,7 @@
 #include <chrono>
 
 int main() {
-    std::mt19937 mt {static_cast<std::mt19937::result_type>(
-    std::chrono::steady_clock::now().time_since_epoch().count()
-    )};
+    std::mt19937 mt {std::random_device{}()};
     std::uniform_int_distribution<int> die {1, 6};
 
     for (int count {1}; count < 10; ++count) {

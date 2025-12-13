3861c22aab56c42d06ce2ba0fea92ef22e1dcf7f
devam <devammondal06@gmail.com>
2025-12-13 06:35:35 -0500

    eof check

--- main.cpp
+++ main.cpp
@@ -12,8 +12,13 @@ int main() {
         std::cin >> a;
 
         if (std::cin.fail()) {
+
+            if (std::cin.eof()) {
+                std::exit(0); //if user enters an EOF character, we exit from the program
+            }
+
             std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             continue;
         } else {
             break;

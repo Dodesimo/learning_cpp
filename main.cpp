712363454845f6d0debdd154d7dfe748cac0721f
devam <devammondal06@gmail.com>
2025-12-15 14:01:20 -0500

    get addresst

--- main.cpp
+++ main.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
 #include <string.h>
 
-void printDouble(double d) {
-    std::cout << d << '\n';
-}
-
-void printDoubleReference(const double& d) {
-    std::cout << d << '\n';
-}
-
 int main() {
     double d {2.0};
-    printDouble(d); //gets copied into here
-    printDoubleReference(d); //pass in by reference, so the parameter is an alias for d
+    std::cout << &d << '\n';
+    std::cout << d << '\n';
 }

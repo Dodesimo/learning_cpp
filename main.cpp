d85094b7b4225b8e6b7f061de8cf7dab37bbf4aa
devam <devammondal06@gmail.com>
2025-12-15 14:41:35 -0500

    null pointer can be changedf

--- main.cpp
+++ main.cpp
@@ -2,24 +2,8 @@
 #include <string.h>
 
 int main() {
-    int a {1};
-    int& refA {a};
-    int* pointA {&a};
-
-    std::cout << a;
-    std::cout << refA;
-    std::cout << *pointA << '\n';
-
-    refA = 2;
-
-    std::cout << a;
-    std::cout << refA;
-    std::cout << *pointA << '\n';
-
-    *pointA = 3;
-
-    std::cout << a;
-    std::cout << refA;
-    std::cout << *pointA << '\n';
-
+    int* ptr {nullptr};
+    int a {4};
+    ptr = &a;
+    std::cout << *ptr << '\n';
 }

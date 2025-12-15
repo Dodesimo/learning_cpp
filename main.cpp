c1b1ea977083af749b7b1b0cbd81eb248688d4de
devam <devammondal06@gmail.com>
2025-12-15 16:46:58 -0500

    const pointer cannot be changed

--- main.cpp
+++ main.cpp
@@ -2,12 +2,7 @@
 #include <string.h>
 
 int main() {
-    int* ptr {nullptr};
-    int a {4};
-    if (ptr == nullptr) {
-        std::cout << "this pointer is null";
-    }
-    if (&a == nullptr) {
-        std::cout << "a's reference is not a null pointer";
-    }
+    int a {2};
+    int* const a_ref {&a};
+    a_ref = nullptr;
 }

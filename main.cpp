3d69067ff6e66aeeb1a9ff28c6ae2de07c9d4de2
devam <devammondal06@gmail.com>
2025-12-15 14:07:42 -0500

    initialize a variable with a pointer

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 
 int main() {
     double d {2.0};
+    double* pointerD {&d};
+
     std::cout << &d << '\n';
-    std::cout << d << '\n';
-    std::cout << *(&d) << '\n';
+    std::cout << pointerD << '\n';
 }

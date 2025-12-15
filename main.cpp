72d83a7d23f2f648c662608922802b5723ca6aea
devam <devammondal06@gmail.com>
2025-12-15 17:50:09 -0500

    default nullptr

--- main.cpp
+++ main.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 #include <string.h>
 
-void printString(std::string s) {
-    std::cout << s << '\n'; //this is expensive because we are doing a direct copy for the parameter
+void printNumber (const int * i = nullptr) {
+    if (i) {
+        std::cout << *i << '\n';
+    } else {
+        std::cout << "nothing was passed in";
+    }
 }
-
-void printStringRef(const std::string& s) {
-    std::cout << s << '\n'; //what gets passed in becomes a reference alias.
-}
-
-void printStringPoint(std::string* s) {
-    std::cout << *s << '\n'; //pass in an address and dereference
-}
-
 int main() {
-    std::string a {"testing!"};
-    printString(a);
-    printStringRef(a);
-    printStringPoint(&a);
+    int a {5};
+    printNumber(&a);
+    int* aNull {};
+    printNumber(aNull);
 }

9ccf938976d41921bcef014b15cce3d615bc1bfe
devam <devammondal06@gmail.com>
2025-12-15 14:43:48 -0500

    null pointer check

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 int main() {
     int* ptr {nullptr};
     int a {4};
-    ptr = &a;
-    std::cout << *ptr << '\n';
+    if (ptr == nullptr) {
+        std::cout << "this pointer is null";
+    }
+    if (&a == nullptr) {
+        std::cout << "a's reference is not a null pointer";
+    }
 }

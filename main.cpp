0b54ad5272bcf16b82da09c6b3262213ac892a1e
devam <devammondal06@gmail.com>
2025-12-15 17:01:18 -0500

    pointer to a constant vs const pointer

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 int main() {
     int a {2};
-    int* const a_ref {&a};
-    a_ref = nullptr;
+    int* aPointer {&a}; //normal pointer, you can change the address and you can change the value
+    const int* aPointerValueConstant {&a}; //pointer to a constant integer, you can't change the value;
+    int* const aPointerPointerConstant {&a}; //the actual pointer is constant.
 }

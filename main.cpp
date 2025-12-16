9019d3bb3810e5145539b216570253de372a5235
devam <devammondal06@gmail.com>
2025-12-16 16:52:30 -0500

    basic struct

--- main.cpp
+++ main.cpp
@@ -2,55 +2,16 @@
 #include <optional>
 #include <string.h>
 
-enum Color {
-    red,
-    blue,
-    yellow,
-};
-
-std::string_view getStringFromColor(Color c) {
-    switch (c) {
-        case red:
-            return "red";
-        case blue:
-            return "blue";
-        case yellow:
-            return "yellow";
-        default:
-            return "Color does not exist";
-    }
-}
-
-std::optional<Color> getColorFromString(std::string_view s) {
-    if (s == "red") {
-        return red;
-    } else if (s == "blue") {
-        return blue;
-    } else if (s == "yellow"){
-        return yellow;
-    }
-
-    return {};
-}
-
-std::ostream& operator<< (std::ostream& out, Color color) {
-    return out << getStringFromColor(color);
-}
-
-std::istream& operator>> (std::istream& input, Color& color) {
-    std::string s {};
-    std::cin >> s;
-    std::optional<Color> value {getColorFromString(s)};
-    if (value) {
-        color = *value;
-        return input;
-    }
-    //if we don't get a match here, the input was invalid
-    input.setstate(std::ios_base::failbit);
-    return input;
-}
-
 int main() {
-    Color c {red};
-    std::cout << c << '\n';
+
+    struct Employee {
+        int age {};
+        double wage {};
+    };
+
+    Employee a {};
+    a.age = 20;
+    a.wage = 3.0;
+
+    std::cout << a.age << '\n' << a.wage;
 }

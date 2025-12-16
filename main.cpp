646621436dbd7ae298c4f1c5817f2526c09ccf39
devam <devammondal06@gmail.com>
2025-12-16 09:27:33 -0500

    prevent namespace colisions through distinct namespaces

--- main.cpp
+++ main.cpp
@@ -2,16 +2,29 @@
 #include <optional>
 #include <string.h>
 
-enum Color {
-    red,
-    blue,
-    white,
-};
+namespace Color {
+    enum Color {
+        red,
+        blue,
+        white,
+    };
+}
+
+namespace Emotion {
+    enum Emotion {
+        red, //prevents naming collision through namespace definition
+        blue, //prevents naming collision through namespace definition
+        yellow,
+    };
+}
+
 int main() {
-    Color a {red};
-    Color b {blue};
-    Color c {white};
+    Color::Color a {Color::red};
+    Color::Color b {Color::red};
+    Color::Color c {Color::red};
+    Emotion::Emotion d {Emotion::blue};
     std::cout << a << '\n';
     std::cout << b << '\n';
     std::cout << c << '\n';
+    std::cout << d << '\n';
 }

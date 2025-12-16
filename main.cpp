807653064149390b5c6eb63c5942ce882ab1a4e3
devam <devammondal06@gmail.com>
2025-12-16 17:14:59 -0500

    default member initializer

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@ int main() {
 
     struct Employee {
         int age {};
-        double wage {};
+        double wage {78.000}; //this is a default member initializer
     };
 
-    Employee a {2, 3.0};
+    Employee a {2};
     std::cout << a.age;
 }

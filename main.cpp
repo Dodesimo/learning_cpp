826e7ee6f430e6bd7d148dae29007acbe9df6599
devam <devammondal06@gmail.com>
2025-12-16 22:24:41 -0500

    pointer

--- main.cpp
+++ main.cpp
@@ -1,32 +1,15 @@
 #include <iostream>
 #include <optional>
 #include <string.h>
-
-struct Fraction {
-    int numerator {};
-    int denominator {};
+struct Employee {
+    int age {2};
+    double wage {100.00};
 };
 
-Fraction multiply (Fraction& a, Fraction&b) {
-    return Fraction{a.numerator * b.numerator, a.denominator * b.denominator};
-}
-
 int main() {
-    Fraction a {};
-    Fraction b {};
-
-    std::cout << "Enter a value for the numerator: ";
-    std::cin >> a.numerator;
-    std::cout << "Enter a value for the denominator: ";
-    std::cin >> a.denominator;
-    std::cout << "";
-    std::cout << "Enter a value for the numerator: ";
-    std::cin >> b.numerator;
-    std::cout << "Enter a value for the denominator: ";
-    std::cin >> b.denominator;
-
-    Fraction c {multiply(a, b)};
-    std::cout << "Your fractions multiplied together: " << c.numerator << '/' << c.denominator;
+    Employee e {};
+    Employee* ePointer {&e};
+    std::cout << ePointer -> wage;
 
     return 0;
 }

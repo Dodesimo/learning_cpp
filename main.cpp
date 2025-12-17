63802bd23fc3f8b2b19886187aec283a5b665c42
devam <devammondal06@gmail.com>
2025-12-16 22:54:11 -0500

    class template for struc

--- main.cpp
+++ main.cpp
@@ -2,12 +2,23 @@
 #include <optional>
 #include <string.h>
 template <typename T>
-struct Test {
+struct Pair {
     T first {};
     T second {};
 };
 
+template <typename T>
+constexpr T max(Pair<T> p) {
+    if (p.first < p.second) {
+        return p.second;
+    }
+    return p.first;
+}
+
 int main() {
-    Test<int> a {};
+    Pair<int> p1 {5, 6};
+    std::cout << max<int>(p1) << '\n';
+    Pair<double> p2 {5.2, 6.2};
+    std::cout << max<double>(p2) << '\n';
     return 0;
 }

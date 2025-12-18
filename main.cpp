cba3995d9ce1a30cb89f6a50568afe9e79bc39f3
devam <devammondal06@gmail.com>
2025-12-18 16:45:18 -0500

    implicit defualt constructor

--- main.cpp
+++ main.cpp
@@ -4,21 +4,8 @@
 class Foo {
     int x {};
     int y {};
-public:
-    Foo() {
-        std::cout << "default constructor, takes no parameters";
-    }
-    Foo(int x, int y):
-    x {x},
-    y {x}
-    {
-        std::cout << "initialize with variables";
-    }
-    Foo(int x = 2, int y = 2) {
-        std::cout << "default arguments provided";
-    }
 };
 
 int main() {
-    return 0;
+    Foo f {}; //implcit default constructor
 }

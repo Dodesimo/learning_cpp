016695a2b11044b97712690e6a8b7b37818ef26d
devam <devammondal06@gmail.com>
2025-12-19 13:49:42 -0500

    copy construcotr

--- main.cpp
+++ main.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
-#include <string.h>
 
-class IntPair {
-    int a {};
-    int b {};
+class Fraction {
+    int n {};
+    int d {};
 public:
-    IntPair(int x, int y):
-    a {x},
-    b {y} {
+    Fraction(int n_p = 0, int n_d = 1):
+    n {n_p},
+    d {n_d}
+    {
+
+    }
+    //copy constructor
+    Fraction(const Fraction& fraction):
+    n {fraction.n},
+    d {fraction.d}
+    {
     }
 
-    int getA() const {return a;}
-    int getB() const {return b;}
+    int getN() const {return n;}
+    int getD () const {return d;}
+
 };
 
-void print(IntPair p)
-{
-    std::cout << "(" << p.getA() << ", " << p.getB() << ")\n";
+void print(const Fraction& f) {
+    std::cout << f.getN() << "/" << f.getD();
 }
 
-
 int main() {
-    print(IntPair{2, 3});
+    Fraction f {2, 3};
+    Fraction copy {f};
+    print(f);
 }

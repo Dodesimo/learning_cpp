3d9b7bc76825b7b1bd28b99fc702d4896dce9a38
devam <devammondal06@gmail.com>
2025-12-20 15:48:06 -0500

    static getter function

--- main.cpp
+++ main.cpp
@@ -1,37 +1,13 @@
 #include <iostream>
 
-template <typename U, typename V, typename Z>
-class Triad {
-    U a {};
-    V b {};
-    Z c {};
+class Test {
+    static inline int a {4};
 public:
-    Triad(const U& aP, const V& bP, const Z& cP):
-    a {aP},
-    b {bP},
-    c {cP}
-    {
-    }
-    void print() const {
-        std::cout << "[" << a << "," << b << "," << c << "]";
-    }
-    const U& first() const {
+    static int getA() {
         return a;
     }
 };
 
-
-int main()
-{
-    Triad<int, int, int> t1{ 1, 2, 3 };
-    t1.print();
-    std::cout << '\n';
-    std::cout << t1.first() << '\n';
-
-    using namespace std::literals::string_literals;
-    const Triad t2{ 1, 2.3, "Hello"s };
-    t2.print();
-    std::cout << '\n';
-
-    return 0;
+int main() {
+    std::cout << Test::getA();
 }

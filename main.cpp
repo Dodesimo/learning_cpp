3e0e51bcd4ef826a950f2cbccfa2d24e797b597f
devam <devammondal06@gmail.com>
2025-12-20 15:55:18 -0500

    declaring void function outside of class

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 
-class Test {
-    static inline int a {4};
+class IdGenerator {
+    static inline int id {0};
 public:
-    static int getA() {
-        return a;
-    }
+    static void incrementId();
+    static int getId(){return id;}
 };
 
+void IdGenerator::incrementId() {
+    ++id;
+}
+
 int main() {
-    std::cout << Test::getA();
+    IdGenerator::incrementId();
+    std::cout << IdGenerator::getId() << '\n';
+    IdGenerator::incrementId();
+    IdGenerator::incrementId();
+    std::cout << IdGenerator::getId();
+    return 0;
 }

019e729f475f3d4366fb8becc5a39a188194222b
devam <devammondal06@gmail.com>
2025-12-19 20:03:34 -0500

    nested type

--- main.cpp
+++ main.cpp
@@ -1,34 +1,21 @@
 #include <iostream>
 
-class Calculator {
-    int acc {};
+class Fruit {
 public:
-    //constructors and functions are declared
-    Calculator();
-    Calculator& add(int x);
-    Calculator& subtract(int x);
-    int getValue() const {return acc;}
-    void reset() {*this = {};}
+    enum Type {
+        apple,
+        banana,
+        orange
+    };
+    Fruit(Type t){this->t = t;}
+    Type& getType(){return t;}
+    void setType(const Type& t){this->t = t;}
+private:
+    Type t {};
 };
 
-//definition of constructor
-Calculator::Calculator(){}
-
-//definition
-Calculator& Calculator::add(int x)
-{
-    acc += x;
-    return *this;
-}
-
-Calculator& Calculator::subtract(int x)
-{
-    acc -= x;
-    return *this;
-}
-
 int main() {
-    Calculator c {};
-    c.add(2).multiply(3).add(5).multiply(2);
-    std::cout << c.getValue();
+    Fruit f {Fruit::apple};
+    std::cout << f.getType();
+    return 0;
 }

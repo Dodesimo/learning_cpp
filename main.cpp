8759a2702e983d83ef7e616ee6a71ced6be9e3cf
devam <devammondal06@gmail.com>
2025-12-21 09:44:26 -0500

    best way to go over a collection without changing the elements

--- main.cpp
+++ main.cpp
@@ -3,12 +3,9 @@
 //friending member functions
 
 int main() {
-    std::vector scores {1, 2, 5, 10, 23, 2};
-    int total {};
-    for (auto i {0}; i < scores.size(); ++i) {
-        total += scores[i];
+    std::vector col {"data", "test", "oof"}; //ctad string
+    for (const std::string& s: col) {
+        std::cout << s << "\n";
     }
-    double avg {static_cast<double>(total) / static_cast<int>(scores.size())};
-    std::cout << avg;
     return 0;
 }

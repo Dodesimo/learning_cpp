305588138d74074d4ad45a0c694f6e987fd364b5
devam <devammondal06@gmail.com>
2025-12-21 07:34:04 -0500

    get vector size type

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 //friending member functions
 
 int main() {
-    std::vector<int> (10);
+    std::vector vec {1, 2, 3, 5, 3}; //ctad
+    std::cout << typeid(std::size(vec)).name();
     return 0;
 }

b980e49e3eef0a9d2b04b190599674decf88b0c2
devam <devammondal06@gmail.com>
2025-12-21 07:36:44 -0500

    at does bounds checkiing

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 int main() {
     std::vector vec {1, 2, 3, 5, 3}; //ctad
-    std::cout << vec.size();
+    std::cout << vec.at(0); //this will work
+    std::cout << vec.at(15141); //this won't work because we will get a bounds check
     return 0;
 }

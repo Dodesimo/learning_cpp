330f93204cb5d9c9b7db34b9dcac1f8fb3e4a976
devam <devammondal06@gmail.com>
2025-12-20 21:06:35 -0500

    vector initialization

--- main.cpp
+++ main.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
+#include <vector>
 //friending member functions
-class Employee {
-    std::string name {};
-public:
-    Employee(std::string_view name):
-    name {name}{}
-    const std::string& getName() const & { //this tells us to call this version of function when called on an l value (makes sense, we want to return by reference and prevent a dangling reference)
-        return name;
-    }
-    const std::string getName() const && { //this tells us to call this version of the function when called on an r value (makes sense, we don't want to return a reference bcause it could dangle)
-        return name;
-    }
-};
+
 int main() {
+    std::vector v {1, 2, 3, 4}; //ctad
+    std::cout << v[0];
     return 0;
 }

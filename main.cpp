95e2785ece051c9e294515e41d06de2f753b0db9
devam <devammondal06@gmail.com>
2025-12-21 18:05:03 -0500

    array declaration (fixed size)

--- main.cpp
+++ main.cpp
@@ -3,14 +3,5 @@
 #include <ranges>
 
 int main() {
-    std::vector<int> stack {}; //can't use ctad because we don't want to initialize this yet
-    stack.reserve(4);
-    std::cout << "capacity: " << stack.capacity() << " ";
-    std::cout << "length: " << stack.size();
-    std::cout << "\n";
-    stack.push_back(5);
-    std::cout << "capacity: " << stack.capacity() << " ";
-    std::cout << "length: " << stack.size();
-    std::cout << "\n";
-
+    std::array<int, 5> a {}; //array declaration
 }

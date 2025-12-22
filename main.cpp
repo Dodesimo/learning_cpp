44cb297049ae8847d4fd642a31877aecd07cf8d2
devam <devammondal06@gmail.com>
2025-12-22 18:37:26 -0500

    iterating through an arr

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 #include <ranges>
 
 int main() {
-    std::array arr {1, 2, 3, 5}; //ctad
-    std::cout << arr.size() << '\n';
-    constexpr int length {arr.size()}; //return value is constexpr, std::size_t  can thus be converted to int. even then, it fits in teh range
+    constexpr std::array arr {1, 2, 3, 5}; //ctad
+    for (std::size_t i {}; i < arr.size(); ++i) {
+        std::cout << arr[i];
+    }
 }

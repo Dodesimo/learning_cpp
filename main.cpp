3f7564deb4c8673d594dff9da12b436db4be931c
devam <devammondal06@gmail.com>
2025-12-22 14:16:31 -0500

    initialization with std::array

--- main.cpp
+++ main.cpp
@@ -4,4 +4,7 @@
 
 int main() {
     std::array<int, 5> a {}; //array declaration
+    std::cout << a[0] << '\n';
+    std::array<int, 5> b; //elements are not defaulted out
+    std::cout << b[0];
 }

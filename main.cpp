00ed17df1756d29170c900cacbbd9a2ca59e64c7
devam <devammondal06@gmail.com>
2025-12-22 18:54:42 -0500

    pass by reference templates all arrays

--- main.cpp
+++ main.cpp
@@ -2,13 +2,16 @@
 #include <vector>
 #include <ranges>
 
-void passByReference(const std::array<int, 5>& a) {
-    for (auto i{0}; i < a.size(); ++i) {
+template <typename U, std::size_t N> //U is a type parameter, N is a non-type parameter
+void passByReference(const std::array<U, N>& a) {
+    for (auto i{0}; i < N; ++i) {
         std::cout << a[i];
     }
 }
 
 int main() {
     constexpr std::array arr {1, 2, 3, 5, 7}; //ctad
+    constexpr std::array arr2 {"testing", "testing", "testing"}; //ctad will deduce this of type <const char*, 3>?
     passByReference(arr);
+    passByReference(arr2);
 }

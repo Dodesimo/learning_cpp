8f47cd64e59d8a668f4feee4b85fd9e754bdd7e2
devam <devammondal06@gmail.com>
2025-12-23 13:49:30 -0500

    reference wrapper

--- main.cpp
+++ main.cpp
@@ -1,18 +1,14 @@
 #include <array>
 #include <iostream>
 
-struct House {
-    int number {};
-    int stories {};
-    int roomsPerStory {};
-};
-
 int main() {
 
-    constexpr std::array h {
-                    House {2, 5, 3},
-                    House {4, 6, 1},
-                    House {122, 1, 2}
-                };
-     //here we provide template arguments, but each of the inner items don't have arguments
+    int x {5};
+    int y {5};
+    int z {5}; //three integer values
+
+    std::array<std::reference_wrapper<int>, 3> arr {x, y, z}; //there are three reference alues
+    arr[0].get() = 2; //change the value the reference points at.
+
+    return 0;
 }

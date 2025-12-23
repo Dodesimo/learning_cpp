1fc22fbf66913fbd1e4460bffad0168f5d1fad9f
devam <devammondal06@gmail.com>
2025-12-23 15:13:29 -0500

    can omit the length

--- main.cpp
+++ main.cpp
@@ -7,6 +7,6 @@ int main() {
     int second_arr[15]{}; //array of 15 elements that are value initialized with 0
     std::cout << second_arr[0];
 
-    int third_arr[3] {1, 2, 3}; //aggregate initialization via an initializer list
+    int third_arr[] {1, 2, 3}; //aggregate initialization via an initializer list, can ommit lenght
     return 0;
 }

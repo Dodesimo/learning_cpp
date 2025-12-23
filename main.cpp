40572829e558e14b67012e6d8fb3f5f281cc350d
devam <devammondal06@gmail.com>
2025-12-23 14:06:19 -0500

    use array and return string representation for enum

--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
 #include <array>
 #include <iostream>
 
+namespace Color {
+    enum Type {
+        red,
+        blue,
+        white,
+    };
+
+    using namespace std::string_view_literals;
+    constexpr std::array strings {"red"sv, "blue"sv, "white"sv};
+
+}
+
+constexpr std::string_view getString(Color::Type e) {
+    return Color::strings[e];
+}
+
 int main() {
-
-    int x {5};
-    int y {5};
-    int z {5}; //three integer values
-
-    std::array<std::reference_wrapper<int>, 3> arr {x, y, z}; //there are three reference alues
-    arr[0].get() = 2; //change the value the reference points at.
-
-    std::reference_wrapper<const int> constReference {std::cref(x)};
-    std::reference_wrapper<int> reference {std::ref(x)};
-
-    return 0;
+    Color::Type c {};
+    std::cout << getString(c);
 }

0c3fb7f2dc28a8e9d6fa37431a0a6eb73f361ab9
devam <devammondal06@gmail.com>
2025-12-24 16:09:28 -0500

    entire dynamic memory allocation process

--- main.cpp
+++ main.cpp
@@ -2,14 +2,9 @@
 #include <iostream>
 #include <cstring>
 
-void doubleNumber(int& i) {
-    i *= 2;
-}
-
 int main() {
-    std::array arr {2, 1, 17, 45, 8, 30};
-    std::for_each(std::begin(arr), std::end(arr), doubleNumber);
-    for (const auto& a : arr) {
-        std::cout << a << '\n';
-    }
+    int* ptr {new int {5}}; // dynamic memory request;
+    std::cout << *ptr;
+    delete ptr; //return memory back to the system
+    ptr = nullptr; //set the ptr to a default value
 }

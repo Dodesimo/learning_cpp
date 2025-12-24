7533e088ddc5a62cb121e0e37e4a878f3f78b059
devam <devammondal06@gmail.com>
2025-12-24 13:49:37 -0500

    use std::find

--- main.cpp
+++ main.cpp
@@ -4,9 +4,10 @@
 
 int main() {
     std::array arr {2, 1, 17, 45, 8, 30};
-    auto start {std::begin(arr)}; //returns a Container::iterator or a Container::const_iterator
-    auto end (std::end(arr)); //returns a Container::iterator or a Container::const_iterator
-    for (auto s {start}; s != end; ++s){
-        std::cout << *s << '\n'; //dereference iterator type
+    auto target {std::find(std::begin(arr), std::end(arr), 8)};
+    if (target == arr.end()) {
+        std::cout << "not found";
+    } else {
+        std::cout << *target; //dereference
     }
 }

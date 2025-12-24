ba1048e3b57c4b1dea568844dc7f6d39de1a115e
devam <devammondal06@gmail.com>
2025-12-23 20:17:45 -0500

    C-style string

--- main.cpp
+++ main.cpp
@@ -2,11 +2,6 @@
 
 int main() {
     //iterating a C-style array through pointer arithmetic.
-    const int array[] {2, 3, 4, 5, 6};
-    const int* start = &array[0];
-    const int* end = start + std::size(array);
-    while (start < end) {
-        std::cout << *start <<'\n';
-        ++start;
-    }
+    const char str[] {"string"};
+    return 0;
 }

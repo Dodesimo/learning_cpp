b5bbceda26a418612a2a50ac63995f22f389739d
devam <devammondal06@gmail.com>
2025-12-24 16:25:06 -0500

    delete

--- main.cpp
+++ main.cpp
@@ -9,5 +9,6 @@ int main() {
     int* array { new int[length]{} }; //iniitalize an array of length length, value init every element.
     array[3] = 2;
     std::cout << array[3];
+    delete[] array;
     return 0;
 }

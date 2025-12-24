122f8b4a7d73cedce7dffba4c55f485b8dbec8cc
devam <devammondal06@gmail.com>
2025-12-24 17:39:32 -0500

    how a pointer to a poitner works

--- main.cpp
+++ main.cpp
@@ -2,24 +2,10 @@
 #include <iostream>
 #include <cstring>
 
-class IntArrayWrapper {
-    int* arrayPointer{};
-    int arrayLength{};
-public:
-     IntArrayWrapper(int size) {
-         arrayLength = size;
-         arrayPointer = new int[static_cast<std::size_t>(arrayLength)]{};
-    }
-    ~IntArrayWrapper() {
-         delete[] arrayPointer;
-     }
-    int getValue(int index){return *(arrayPointer + index);}
-    void setValue(int index, int value){*(arrayPointer + index) = value;}
-};
-
 int main() {
-    IntArrayWrapper i {10};
-    std::cout << i.getValue(0) << '\n';
-    i.setValue(0, 232);
-    std::cout << i.getValue(0);
+    int v {5};
+    int* vPointer {&v};
+    int** vPointerPointer {&vPointer};
+    std::cout << *vPointer;
+    std::cout << **vPointerPointer;
 }

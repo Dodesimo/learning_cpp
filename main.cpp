887aca1cdc5c1821c0cdeecc02f488d2469e9046
devam <devammondal06@gmail.com>
2025-12-24 13:28:40 -0500

    selection sort, decreasing

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <array>
 #include <iostream>
 #include <cstring>
-#include <algorithm>
 
 int main() {
     std::array arr {2, 1, 17, 45, 8, 30};
-    std::sort(std::begin(arr), std::end(arr)); //specify pointers to the frtn end end of the array
+    for (auto i {0}; i < arr.size(); ++i) {
+        int maximumIndex {i};
+        for (auto j {i + 1}; j < arr.size(); ++j) {
+            if (arr[j] > arr[maximumIndex]) {
+                maximumIndex = j;
+            }
+        }
+        std::swap(arr[i], arr[maximumIndex]);
+    }
 
     for (const auto& a : arr){std::cout << a << '\n';}
 

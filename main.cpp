fb867bfe00879b13dc4997f6628ea5f9c6b99ef3
devam <devammondal06@gmail.com>
2025-12-24 13:26:44 -0500

    fixed

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@
 int main() {
     std::array arr {2, 1, 17, 45, 8, 30};
     for (auto i {0}; i < arr.size(); ++i) {
-        int minimum {std::numeric_limits<int>::max()};
+        int minimumIndex {i};
         for (auto j {i + 1}; j < arr.size(); ++j) {
-            if (arr[j] < minimum) {
-                minimum = arr[j];
+            if (arr[j] < arr[minimumIndex]) {
+                minimumIndex = j;
             }
         }
-        std::swap(arr[i], minimum);
+        std::swap(arr[i], arr[minimumIndex]);
     }
 
     for (const auto& a : arr){std::cout << a << '\n';}

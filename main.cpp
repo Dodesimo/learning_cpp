53a894a12a8e07c1f86ab5557908e3843af55b81
devam <devammondal06@gmail.com>
2025-12-24 17:50:02 -0500

    making a two dimensional array

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 
 int main() {
     int** array {new int*[10]}; //this is a pointer to a pointer because arrays decay into a pointer to their first element.
+    for (auto i {0}; i < 10; ++i) {
+        array[i] = new int[5]{}; //value initalize each of the elements,
+    }
+    array[2][1] = 4;
+    std::cout << array[2][1];
     delete[] array;
     return 0;
 }

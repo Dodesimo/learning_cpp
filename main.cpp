deacb3d51ce569b497389454397e900998f1b6c4
devam <devammondal06@gmail.com>
2025-12-24 13:27:52 -0500

    std::sort

--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
 #include <array>
 #include <iostream>
 #include <cstring>
+#include <algorithm>
 
 int main() {
     std::array arr {2, 1, 17, 45, 8, 30};
-    for (auto i {0}; i < arr.size(); ++i) {
-        int minimumIndex {i};
-        for (auto j {i + 1}; j < arr.size(); ++j) {
-            if (arr[j] < arr[minimumIndex]) {
-                minimumIndex = j;
-            }
-        }
-        std::swap(arr[i], arr[minimumIndex]);
-    }
+    std::sort(std::begin(arr), std::end(arr)); //specify pointers to the frtn end end of the array
 
     for (const auto& a : arr){std::cout << a << '\n';}
 

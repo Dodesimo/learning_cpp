204b2a53052f07151cea3bc748fd59f60cddf5f0
devam <devammondal06@gmail.com>
2025-12-23 20:28:24 -0500

    can change individual characters

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 int main() {
     //iterating a C-style array through pointer arithmetic.
-    const char str[] {"string"};
+    char str[] {"string"};
+    str[0] = 'a';
+    std::cout << str;
     return 0;
 }

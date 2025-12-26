43fc8d696be389c5172be588bd39be3474b3daef
devam <devammondal06@gmail.com>
2025-12-26 13:49:55 -0500

    function pointers

--- main.cpp
+++ main.cpp
@@ -2,9 +2,18 @@
 #include <iostream>
 #include <cstring>
 
+int func () {
+    return 5;
+}
+
+int goo () {
+    return 4;
+}
+
 int main() {
-    int a {5};
-    void* aPointer {&a}; //so a void pointer containing the address of a.
-    std::cout << *static_cast<int*>(aPointer); //before dereferencing, need to get it to the right type.
+    int (*f)() {&func};
+    std::cout << reinterpret_cast<void*>(f) << '\n';
+    f = &goo;
+    std::cout << reinterpret_cast<void*>(f) << '\n';
     return 0;
 }

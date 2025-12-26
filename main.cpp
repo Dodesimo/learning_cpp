ccbcbfdff4c74ece4027892b6b82f42951c046aa
devam <devammondal06@gmail.com>
2025-12-26 16:10:33 -0500

    use a function pointer to make choosing a parameter less ambigious

--- main.cpp
+++ main.cpp
@@ -2,16 +2,16 @@
 #include <iostream>
 #include <cstring>
 
-int func () {
-    return 5;
+void print(int x) {
+    std::cout << x;
 }
 
-int goo () {
-    return 4;
+void print(int x, int y = 10) {
+    std::cout << x << '\n' << y;
 }
 
 int main() {
-    int (*f)() {&func};
-    f();
+    void (*fn)(int) {print}; //we make it less ambiguious what we want to call
+    fn(10);
     return 0;
 }

1f9acdc8e04ba781e3348568487e38ad0cada5a7
devam <devammondal06@gmail.com>
2025-12-26 19:29:18 -0500

    can initialize a function poitner with lambda

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 #include <cstdarg>
 
 int main(int argc, char* argv []) {
-    auto isEven {
+    bool (*isEven)(int i) {
         [](int i) -> bool {return i % 2 == 0;}
     };
-    std::cout << std::boolalpha << isEven(5);
+    std::cout << std::boolalpha << isEven(5); //
     return 0;
 }

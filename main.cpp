bfca9b40bdfd0f12816707aaee7ff3734e9185cf
devam <devammondal06@gmail.com>
2025-12-26 20:30:05 -0500

    mutable but only changes local lambda variable

--- main.cpp
+++ main.cpp
@@ -3,24 +3,19 @@
 #include <cstdarg>
 #include <functional>
 
-struct Student {
-    std::string name {};
-    int points{};
-};
-
 int main(int argc, char* argv []) {
-    std::array<Student, 8> arr{
-              { { "Albert", 3 },
-                { "Ben", 5 },
-                { "Christine", 2 },
-                { "Dan", 8 }, // Dan has the most points (8).
-                { "Enchilada", 4 },
-                { "Francis", 1 },
-                { "Greg", 3 },
-                { "Hagrid", 5 } }
+
+    int var {1};
+    auto lambda {
+        [var]() mutable {
+            ++var;
+            std::cout << "var is now: " << var << '\n';
+        }
     };
 
-    const auto largest {std::max_element(arr.begin(), arr.end(), [](const Student& a, const Student& b){return a.points < b.points;})};
-    if (largest == arr.end()) {std::cout << "not found";}
-    std::cout << (*largest).name;
+    lambda();
+    lambda();
+    std::cout << var;
+    return 0;
+
 }

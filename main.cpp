72821fc8cea7bfba4339ae77d68dd7d254600020
devam <devammondal06@gmail.com>
2025-12-28 03:44:04 -0500

    different types of overloads

--- main.cpp
+++ main.cpp
@@ -1,22 +1,33 @@
 #include <array>
 #include <iostream>
 
-class Matrix {
-    double data[4][4] {}; //four by four matrix value initialized
+class Fraction {
+    int n {};
+    int d {};
 public:
-    double operator()(int row, int column) const; //this is a const overload of () that allows for indexing on const matrices
-    double& operator()(int row, int column); //overload that works for value changing
+    Fraction(int x, int y): n {x}, d {y} {}
+    Fraction (const Fraction& f) {n = f.n; d = f.d;} //this is a copy constructor, if we do = default will by default do a value initalization
+    //overload the copy assignment
+    Fraction& operator= (const Fraction& f) {
+       n = f.n;
+       d = f.d; 
+       return *this;
+    }
+    //overload the extraction operator
+    friend std::ostream& operator<< (std::ostream& s, const Fraction& f);
 };
 
-double& Matrix::operator()(int row, int column){
-    return data[row][column]; //do some bounds checking
+std::ostream& operator<< (std::ostream& s, const Fraction& f){
+    s << f.n << " " << f.d << '\n';
+    return s;
 }
-
-double Matrix::operator()(int row, int column){
-    return data[row][column]; //do some bounds checking
-}
-
 
 int main() {
+    Fraction f {1, 2};
+    std::cout << f;
+    Fraction d {f}; //copy constrtuctor invoked
+    std::cout << d;
+    Fraction e  = f; //copy assigmnent used
+    std::cout << e;
     return 0;
 }

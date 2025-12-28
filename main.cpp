a18956db004300328f545ce34263f3775bdc9c70
devam <devammondal06@gmail.com>
2025-12-27 19:51:34 -0500

    overload <<

--- main.cpp
+++ main.cpp
@@ -6,16 +6,18 @@ class Cents {
 public:
     Cents(int cents): mCents {cents}{}
     int getCents() const {return mCents;}
-    friend std::ostream& operator<< (std::ostream& out, const Cents& c){
-        std::cout << "The cents is: " << c.getCents();
-        return out;
-    }
 };
 
 //bceause this is a friend, don't need to do scope reoslution
 int operator+(const Cents& c1, const Cents& c2){
     return c1.getCents() + c2.getCents();
 }
+
+std::ostream& operator<< (std::ostream& out, const Cents& c){
+    std::cout << "The cents is: " << c.getCents();
+    return out;
+}
+
 
 int main(int argc, char* argv []) {
     Cents c1 {2};

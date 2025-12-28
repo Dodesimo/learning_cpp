8852f0e09e81ccee5b476fef7b5f3da45b3c1b96
devam <devammondal06@gmail.com>
2025-12-27 19:37:22 -0500

    not using a friend function

--- main.cpp
+++ main.cpp
@@ -6,17 +6,17 @@ class Cents {
 public:
     Cents(int cents): mCents {cents}{}
 //use a friend function to give access to internal of cents and overload the + operator
-friend int operator+ (const Cents& c1, const Cents& c2);
 int getCents() const {return mCents;}
 };
 
 //bceause this is a friend, don't need to do scope reoslution
 int operator+(const Cents& c1, const Cents& c2){
-    return c1.mCents + c2.mCents;
+    return c1.getCents() + c2.getCents();
 }
 
 int main(int argc, char* argv []) {
     Cents c1 {2};
     Cents c2 {4};
-    std::cout << c1 + c2;
+    Cents c3 {c1 + c2};
+    std::cout << c3.getCents();
 }

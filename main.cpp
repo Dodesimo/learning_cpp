923ddf4824c9ac4ac4a568b6dd1c1ccf5d888ae8
devam <devammondal06@gmail.com>
2025-12-29 16:35:18 -0500

    dereferencing smart pointer

--- main.cpp
+++ main.cpp
@@ -2,21 +2,24 @@
 #include <iostream>
 #include <algorithm>
 
-template <typename T>
-void swap(T& a, T& b){
-    T temp {std::move(a)};
-    a = std::move(b);
-    b = std::move(temp); //no deep copies
-}
+class Resource {
+    int a {2};
+public:
+    Resource() {std::cout << "Allocated resource" << '\n';}
+    ~Resource() {std::cout << "deleted resource" << '\n';}
+    friend std::ostream& operator<< (std::ostream& s, const Resource& r){
+        std::cout << r.a;
+        return s;
+    }
+};
 
 int main() {
-    std::string x {"abc"};
-    std::string y {"efg"};
-    
-    std::cout << "before swap" << '\n';
-    std::cout << x << ' ' << y;
-    swap(x, y);
-    std::cout << "after" << '\n';
-    std::cout << x << ' ' << y;
+    std::unique_ptr<Resource> up {new Resource{}}; //takes a template name
+    std::unique_ptr<Resource> newUp {};
+
+    newUp = std::move(up); 
+    if (newUp){
+        std::cout << *newUp;
+    }
     return 0;
 }

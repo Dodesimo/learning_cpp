9f11c1e6d732a2bafafd2f5657ce01b6002edb7c
devam <devammondal06@gmail.com>
2025-12-28 19:13:11 -0500

    primitive move semantics

--- main.cpp
+++ main.cpp
@@ -6,7 +6,22 @@ class SmartPointer{
     T* internalPointer {};
 public:
     SmartPointer(T* pointer = nullptr): internalPointer {pointer} {};
+    SmartPointer(const SmartPointer& other){
+        internalPointer = other.internalPointer; //ddon't need to delete the internalPointer because we are initializeing a new object
+        other.internalPointer = nullptr; //implement move semantics, move the internal pointer over to the present one. 
+    }
     ~SmartPointer() {delete internalPointer;}
+    SmartPointer& operator=(SmartPointer s other){
+        delete internalPointer; //beacuse we do'nt know what data is already there. 
+        internalPointer = other.internalPointer;
+        other.internalPointer = nullptr;
+    }
+    SmartPointer& operator= (SmartPointer s){
+        if (this == &s){
+            return *this; //just return the current element.
+        }
+
+    }
     //overload the dereference operator
     T operator*() const {
         return *internalPointer;

a82661fe329597f88f594f1cee406b178a85542e
devam <devammondal06@gmail.com>
2025-12-29 19:26:53 -0500

    added

--- main.cpp
+++ main.cpp
@@ -2,18 +2,25 @@
 #include <iostream>
 #include <algorithm>
 
-class Point2d{
-    int x {};
-    int y {};
+class Teacher {
+    std::string name;
 public:
-    Point2d(int x, int y): x {x}, y {y} {}
+    Teacher(std::string_view s): name {s} {};
+    const std::string_view getName() const {return name;}
 };
 
-class Line{
-    Point2d first {};
-    Point2d second {}; //composition
+class Department {
+    const Teacher& t; //has a reference to teacher, doesn't actually own it?
+public:
+    Department(const Teacher& teacher): t {teacher}{}
 };
 
 int main() {
+    Teacher t {"test"};
+    {
+        Department d {t}; //d doesn't own t
+    }
+    //t still exists
+    std::cout << t.getName();
     return 0;
 }

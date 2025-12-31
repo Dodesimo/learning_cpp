602989919c67ca8c775e7db17b9b91efe0000d3a
devam <devammondal06@gmail.com>
2025-12-31 18:13:52 -0500

    inheritance with constructors

--- main.cpp
+++ main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 
-class A {
+class Person{
+    std::string name {};
+    int age {};
 public:
-    A() {std::cout << "A constructor called" << '\n';}
+    Person() = default;
+    Person(std::string_view n, int a): name {n}, age {a} {}
+    int const getAge() {return age;}
 };
 
-class B : public A {
+class BaseballPlayer : public Person {
+    //public inheritance
+    std::string team {};
+    int bA {};
 public:
-    B() {std::cout << "B constructor called" << '\n';}
+    BaseballPlayer() = default; //will use the default person constructor
+    BaseballPlayer(const std::string_view n, int a, const std::string_view t, int battingAvg):
+    Person(n, a),
+    team {t},
+    bA {a} {}
 };
 
-int main() {
-    A a {};
-    B b {};
+int main(){
+    BaseballPlayer b {}; //call default constructor
+    BaseballPlayer p {"Ted", 30, "Mets", 250};
+
+    std::cout << b.getAge() << '\n';
+    std::cout << p.getAge() << '\n';
     return 0;
 }

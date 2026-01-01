b1d977d5eedf6abfd4286324a46edca640c358e5
devam <devammondal06@gmail.com>
2026-01-01 17:41:14 -0500

    pure virtual function

--- main.cpp
+++ main.cpp
@@ -1,23 +1,20 @@
-#include <string_view>
 #include <iostream>
 
 class Base {
 public:
-    std::string_view getName() const {return "Base";}
-    virtual std::string_view getNameVirtual() const {return "Base";}
+    virtual int getValue() const = 0; //pure virtual function
 };
 
-class Derived : public Base {
+class A : public Base {
 public:
-    std::string_view getName() const {return "Derived";}
-    virtual std::string_view getNameVirtual() const override {return "derived";}
+    virtual int getValue () const override {
+        return 2;
+    }
 };
 
 int main() {
-    Derived derived {};
-    Base& base {derived};
-
-    std::cout << "base has a static type " << base.getName() << '\n'; //should be base
-    std::cout << "base has a dynamic type " << base.getNameVirtual() << '\n'; //should be derived
-
+    A a {};
+    Base& b {a};
+    std::cout << b.getValue(); //will resolve to class A's
+    return 0;
 }

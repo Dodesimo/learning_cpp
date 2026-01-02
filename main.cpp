64cfcd533e2b11a52db90ef98a840ca160473aaf
devam <devammondal06@gmail.com>
2026-01-02 18:59:39 -0500

    class template initialization for bools

--- main.cpp
+++ main.cpp
@@ -13,6 +13,13 @@ public:
     }
 };
 
+template<>
+//class template specialization
+class Storage<bool> { //this is completely different
+public:
+    Storage<bool>() {std::cout << "we are using a class template specialization";}
+};
+
 int main() {
     Storage<int> s {};
     for (auto i {0}; i < 8; ++i) {
@@ -21,4 +28,8 @@ int main() {
     for (auto i {0}; i < 8; ++i) {
         std::cout << s.get(i) << '\n';
     }
+
+    //invokes class creation from teh class template specialization for bool
+    Storage<bool> b {};
+
 }

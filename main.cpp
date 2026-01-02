c63c26464498cf0483983db92cf952c4f5a2d057
devam <devammondal06@gmail.com>
2026-01-02 14:13:03 -0500

    generic array wrapper class

--- main.cpp
+++ main.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 
-class Base {
+template <typename T>
+class GenericArray {
+    T* array {};
+    int length {};
 public:
-    Base() {}
-    int getNumber() {return 2.0;}
-    virtual ~Base() {}
-};
+    GeneralArray(l): length {l} {
+        assert(l > 0); //see if of size
+        array = new T[length] {};
+    }
+    Array (const Array& other) = delete; //disable copy constructor
+    Array& operator=(const Array& other) = delete; //disable copy assignment
 
-class Derived : public Base {
-public:
-    Derived() {};
-    double getDouble() {return 21.0;}
-};
+    ~GeneralArray(){
+        delete[] array;
+    }
 
-int main() {
-    Derived d {};
-    Base* b {&d};
-    std::cout << (dynamic_cast<Derived*>(b))->getDouble();
-}
+    void erase() {
+        delete[] array;
+        array = nullptr;
+        length = 0;
+    }
+
+    int getLength() const {return length;}
+
+    T& operator[] (int index) {
+        return array[index];
+    }
+};

61666ed159a16fb4873a150eacaadcfc3608b74f
devam <devammondal06@gmail.com>
2026-01-03 13:42:40 -0500

    catch with a string handler

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-
 int main() {
+    int number {};
+    std::cout << "Enter a number: ";
+    std::cin >> number;
     try {
-        throw -1;
-    } catch (int x){
-        std::cerr << "error";
+        if (number < 0) {
+            throw "Can't have a negative number!";
+        }
+
+        std::cout << "doubled number: " << number * 2;
+        return 0;
+        
+    } catch (const char* error) {
+
+        std::cout << error << '\n';
+        
     }
 }

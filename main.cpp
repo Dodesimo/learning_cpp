4c206f61c93277ba6cc551391357bef588672d31
devam <devammondal06@gmail.com>
2026-01-03 17:26:47 -0500

    catching error at main level

--- main.cpp
+++ main.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
-#include <exception>
 
-class Array {
-    int array[3] {};
+class A {
+    int member {};
 public:
-    Array() {}
-    void insert(int index, int value){
-        if (index < 0 || index > 2) {
-            throw std::runtime_error{"Out of bounds!"};
+    A (int x): member {x} {
+
+        if (x < 0) {
+            throw 1;
         }
-    }
+
+    };
 };
 
-int main() {
-    Array a {};
-    try {
-        a.insert(5, 3); //this will obviously fail
-    } catch (const std::runtime_error& a){
-        std::cerr << a.what();
-    }
-    return 0;
-}
+class B : public A {
+public:
+    B (int x) : A {x} {}
+};
 
+int main () {
+    try {
+        A a {-5};
+    } catch (int x) {
+        std::cout << x;
+    }
+}

6dfed4788472442a87112e842d8a5e9f581e5d26
devam <devammondal06@gmail.com>
2026-01-03 13:30:25 -0500

    basic try/catch block with a throw

--- main.cpp
+++ main.cpp
@@ -1,35 +1,9 @@
 #include <iostream>
 
-template <typename T>
-class Storage {
-    T array[8];
-public:
-    void set (T index, const T& value) {
-        array[index] = value;
-    }
-
-    const T& get (T index) const{
-        return array[index];
-    }
-};
-
-template<>
-//class template specialization
-class Storage<bool> { //this is completely different
-public:
-    Storage<bool>() {std::cout << "we are using a class template specialization";}
-};
-
 int main() {
-    Storage<int> s {};
-    for (auto i {0}; i < 8; ++i) {
-        s.set(i, i);
+    try {
+        throw -1;
+    } catch (int x){
+        std::cerr << "error";
     }
-    for (auto i {0}; i < 8; ++i) {
-        std::cout << s.get(i) << '\n';
-    }
-
-    //invokes class creation from teh class template specialization for bool
-    Storage<bool> b {};
-
 }

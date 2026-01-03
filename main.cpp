9891382e88d03ee830990b7da1b508f9942e18bc
devam <devammondal06@gmail.com>
2026-01-03 16:29:20 -0500

    custom error class

--- main.cpp
+++ main.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 
-int doubleNumber (int x) {
-    return x * 2;
-}
+class ArrayOutOfBoundsException {
+    std::string message {};
+public:
+    ArrayOutOfBoundsException(std::string_view m): message {m} {}
+    std::string_view getMessage() const {return message;}
+};
+
+class Array {
+    int array[3] {};
+public:
+    Array() {}
+    void insert(int index, int value){
+        if (index < 0 || index > 2) {
+            throw ArrayOutOfBoundsException{"Out of bounds!"};
+        }
+    }
+};
 
 int main() {
-    int n {};
-    std::cin >> n;
-    
+    Array a {};
     try {
-        if (n < 0) {
-            throw "fade";
-        }
-    } catch (...)  {
-        std::cerr << "negative number!";
-    } //catch all exception handler
-
+        a.insert(5, 3); //this will obviously fail
+    } catch (const ArrayOutOfBoundsException& a){
+        std::cerr << a.getMessage();
+    }
     return 0;
 }
+

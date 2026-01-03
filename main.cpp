2cdf7424d56f91152e2f50725a5df7d1755e9f8d
devam <devammondal06@gmail.com>
2026-01-03 16:34:13 -0500

    using a std:: exception class

--- main.cpp
+++ main.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
-
-class ArrayOutOfBoundsException {
-    std::string message {};
-public:
-    ArrayOutOfBoundsException(std::string_view m): message {m} {}
-    std::string_view getMessage() const {return message;}
-};
+#include <exception>
 
 class Array {
     int array[3] {};
@@ -13,7 +7,7 @@ public:
     Array() {}
     void insert(int index, int value){
         if (index < 0 || index > 2) {
-            throw ArrayOutOfBoundsException{"Out of bounds!"};
+            throw std::runtime_error{"Out of bounds!"};
         }
     }
 };
@@ -22,8 +16,8 @@ int main() {
     Array a {};
     try {
         a.insert(5, 3); //this will obviously fail
-    } catch (const ArrayOutOfBoundsException& a){
-        std::cerr << a.getMessage();
+    } catch (const std::runtime_error& a){
+        std::cerr << a.what();
     }
     return 0;
 }

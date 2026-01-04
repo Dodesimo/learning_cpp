f88e952fe839cf902011a4a753723111fd7832be
devam <devammondal06@gmail.com>
2026-01-04 17:00:57 -0500

    extracting by parts from the stream

--- main.cpp
+++ main.cpp
@@ -6,5 +6,10 @@ int main() {
     std::stringstream os {};
     os << "test"; //put in
     os.str("testing more"); //put string literal in
-    std::cout << '\n' << os.str(); 
+    std::string first {};
+    std::string second {};
+    os >> first;
+    os >> second;
+    std::cout << first << '\n';
+    std::cout << second << '\n';
 }

59a92e1e9d6f6ae3fefb7300975552e4ac6ded43
devam <devammondal06@gmail.com>
2026-01-04 16:30:58 -0500

    get number of values inputted through gcount

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 int main() {
     char buf[10] {}; //ten character array
     std::cin.get(buf, 10); //include white spaces and get ten characters
-    std::cout << buf;
+    //use getline to discard new line operator.
+    std::cout << buf << '\n';
+    std::cout << std::cin.gcount() << '\n';
     return 0;
 }

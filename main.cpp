c19e3842aa30e396de534e2826c000b7f457fe45
devam <devammondal06@gmail.com>
2026-01-04 17:23:40 -0500

    remember to clear the buffer after a fail

--- main.cpp
+++ main.cpp
@@ -1,14 +1,30 @@
+#include <algorithm> 
+#include <cctype> 
 #include <iostream>
-#include <iomanip>
-#include <sstream>
+#include <ranges>
+#include <string>
+#include <string_view>
+
+//function to see if a string has all letters, numbers, and spaces
+
+bool isAlphaNum(std::string_view s){
+    return std::all_of(s.begin(), s.end(), [](char c) {return std::isdigit(c) || std::isspace(c);});
+}
 
 int main() {
-    std::stringstream os {};
-    os << "123231" << '\n';
-    os << "129090321" << '\n';
     int a {};
-    int b {};
-    os >> a >> b;
-    std::cout << a << '\n';
-    std::cout << b;
+    while (true) {
+        std::cout << "valid number?" << '\n';
+        std::cin >> a;
+        if (std::cin.fail()) {
+            //reset
+            std::cin.clear();
+            //clear the buffer
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "try again" << '\n';
+            continue;
+        }
+        break;
+    }
+    return 0;
 }

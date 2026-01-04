e243396c476adcff86ad53b5aa5fd2c3dd22f5a4
devam <devammondal06@gmail.com>
2026-01-04 16:37:19 -0500

    format group configuration

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 #include <iomanip>
 
 int main() {
-    std::cout.setf(std::ios::showpos);
-    std::cout << 2;
+    std::cout.setf(std::ios::hex, std::ios::basefield); //within the basefield set of flags, turn everything off and turn hex on
+    std::cout << 28;
 }

5dd0855b347f1ad246a4c2a30c912f6e35999716
devam <devammondal06@gmail.com>
2026-01-04 14:32:53 -0500

    limit buffer input from stream

--- main.cpp
+++ main.cpp
@@ -1,34 +1,8 @@
 #include <iostream>
-#include <stdexcept>
-
-class Fraction {
-    int numerator {};
-    int denominator{};
-
-public:
-    Fraction(int n, int d): numerator {n}, denominator {d} {
-        if (denominator == 0) {
-            throw std::runtime_error("Denominator cannot be 0");
-        }
-    }
-
-    void print() {std::cout << numerator << '/' << denominator;}
-};
+#include <iomanip>
 
 int main() {
-    std::cout << "Enter the numerator: ";
-    int n {};
-    std::cin >> n;
-
-    std::cout << "Enter the denominator: ";
-    int d {};
-    std::cin >> d;
-    
-    try {
-        Fraction f {n, d};
-        f.print();
-    } catch (const std::runtime_error& r) {
-        std::cout << "invalid denominator";
-    }
-
+    char buf[10] {}; //ten character array
+    std::cin >> std::setw(10) >> buf;
+    return 0;
 }

17351d42bdb27432de39054c05c02cd994d715a9
devam <devammondal06@gmail.com>
2026-01-04 16:25:14 -0500

    .get() includes white spaces

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 int main() {
     char buf[10] {}; //ten character array
-    std::cin >> std::setw(10) >> buf;
+    std::cin.get(buf, 10); //include white spaces and get ten characters
+    std::cout << buf;
     return 0;
 }

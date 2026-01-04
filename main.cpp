dd75ad74d8fdc1a3bcc5226fab18411185052abd
devam <devammondal06@gmail.com>
2026-01-04 16:32:22 -0500

    set boolean flag for output

--- main.cpp
+++ main.cpp
@@ -2,10 +2,6 @@
 #include <iomanip>
 
 int main() {
-    char buf[10] {}; //ten character array
-    std::cin.get(buf, 10); //include white spaces and get ten characters
-    //use getline to discard new line operator.
-    std::cout << buf << '\n';
-    std::cout << std::cin.gcount() << '\n';
-    return 0;
+    std::cout.setf(std::ios::showpos);
+    std::cout << 2;
 }

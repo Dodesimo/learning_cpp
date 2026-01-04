049ab3a2cfba883e0eb2314175d65ac118076b94
devam <devammondal06@gmail.com>
2026-01-04 17:32:33 -0500

    writing to file

--- main.cpp
+++ main.cpp
@@ -1,30 +1,15 @@
-#include <algorithm> 
-#include <cctype> 
 #include <iostream>
-#include <ranges>
-#include <string>
-#include <string_view>
-
-//function to see if a string has all letters, numbers, and spaces
-
-bool isAlphaNum(std::string_view s){
-    return std::all_of(s.begin(), s.end(), [](char c) {return std::isdigit(c) || std::isspace(c);});
-}
+#include <fstream>
 
 int main() {
-    int a {};
-    while (true) {
-        std::cout << "valid number?" << '\n';
-        std::cin >> a;
-        if (std::cin.fail()) {
-            //reset
-            std::cin.clear();
-            //clear the buffer
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cout << "try again" << '\n';
-            continue;
-        }
-        break;
+    std::ofstream file {"text.txt"};
+    try {
+        if (!file) {
+            throw "error";
+        } 
+        file << "test \n";
+        file << "test \n";
+     }catch (const char* error) {
+        std::cout << "error opening file";
     }
-    return 0;
 }
